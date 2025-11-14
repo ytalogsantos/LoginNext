500d92221c9ec7c6fc299d8e7a3ad8113a78ba40
Ytalo Santos <ytalos762@gmail.com>
2025-11-14 19:58:01 -0300

    initial commit

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,4 @@
+import LoginForm from "@/app/ui/LoginForm";
+export default function Page() {
+    return <LoginForm />;
+}
---
+++ app/ui/LoginForm.tsx
@@ -0,0 +1,28 @@
+"use client";
+import styles from "@/app/styles/forms.module.css";
+import { montserrat } from "@/app/layout";
+export default function LoginForm() {
+    return (
+        <>
+        <h1>Login</h1>
+        <div>
+            <form className={styles.loginForm}>
+                <div>
+                    <label htmlFor="email" className={montserrat.className}>Email </label>
+                    <input type="email" name="email" id="email-input" required placeholder="youremail@something.com" className={montserrat.className}/>
+                </div>
+                <div>
+                    <label htmlFor="password">Password </label>
+                    <input type="password" name="password" id="password-input" required placeholder="********" className={montserrat.className}/>
+                </div>
+                <div>
+                    <button type="submit" className={montserrat.className}>Log in</button>
+                    <button className={montserrat.className}>Forgot password?</button>
+                    <button className={montserrat.className}>Sign up</button>
+                </div>
+
+            </form>
+        </div>
+        </>
+    );
+}
